d4390aba24562687652d5994b8ab4cfb088feaca
FrankPolicastro <frankpolicastro2016@gmail.com>
2023-11-13 16:44:30 -0500

    Week 5 D3.js exercises
    Week 5 D3.js exercises

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function main() {
         data.forEach(function(row) {
             row.year = +row.year;
             row.daysAfterMidtermsAnnounced = +row.daysAfterMidtermsAnnounced;
+        
         });
 
         drawScatterplot(data);
@@ -17,7 +18,7 @@ function drawScatterplot(data) {
     const margin = {top: 50, right: 50, bottom: 50, left: 50};
     const width = window.innerWidth * 0.5;
     const height = window.innerHeight * 0.5;
-
+    
     // append the svg object to the body of the page
     // document.getElementById("scatterplot") = d3.select("#scatterplot")
 
@@ -25,10 +26,10 @@ function drawScatterplot(data) {
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
+        //.attr("fill", "pink") //why doesn't this work?
         .append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
-        
-
+                
     console.log(d3.extent(data, function(d) { return d.daysAfterMidtermsAnnounced }));
 
     // create x scale
@@ -42,13 +43,13 @@ function drawScatterplot(data) {
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .attr("fill", "pink");
-
+    
     svg.append("g")
         .style("font", "14px times") // increase font FP 11-8-2023
         .attr("class", "axis")
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(xScale));
-
+    
     // create y scale
     const yScale = d3.scaleLinear()
         .domain(d3.extent(data, function(d) { return d.year }))
@@ -59,7 +60,21 @@ function drawScatterplot(data) {
         .attr("class", "axis")
         .attr("transform", `translate(0, 0)`)
         .call(d3.axisLeft(yScale)
-        .tickFormat(d3.format('d')));  //tickFormat to remove commas - FP 11-8-2023
+        //.ticks(4)
+        //.tickFormat(d3.format('d')));  //tickFormat to remove commas - FP 11-8-2023
+        .tickFormat(function(d) {
+            if (d===2022) 
+               { return "" }
+            if (d===2018)
+               { return "" }
+            if (d===2014) 
+               { return "" }
+            if (d===2010)
+               { return "" }
+            else 
+               { return d}
+        }));
+        //.ticks(5)
 
     // add circles
     svg.selectAll(".announcedCircle")
@@ -70,7 +85,9 @@ function drawScatterplot(data) {
         .attr("cx", function(row) { return xScale(row.daysAfterMidtermsAnnounced)})
         .attr("cy", function(row) { return yScale(row.year)})
         .attr("r", 5) // changed teh circle size to '5' - FP 11-8-2023
-        .style("fill", 'blue') // changed the color to 'blue' - FP 11-8-2023
+        //.style("fill", 'blue') // changed the color to 'blue' - FP 11-8-2023
+        // dynamic color change from 'red' to 'blue' - FP 11-13-2023
+        .style("fill", function(row){ return row.party == 'D' ? 'blue' : 'red' })
         .style('opacity', 0.75); 
 }
 
